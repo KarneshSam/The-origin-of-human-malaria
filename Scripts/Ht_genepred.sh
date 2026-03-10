2dca0eadbbb44616a75decd951334e6c1fe76579
Karnesh Sampath <inf-41-2025@bioinf-serv2.cob.lu.se>
2026-03-10 03:07:25 +0100

    updated script by modifing the path and file basename

--- Scripts/Ht_genepred.sh
+++ Scripts/Ht_genepred.sh
@@ -1,18 +1,21 @@
 # filter the bird genome from the parasite using gc content and remove the contig with sequence length less than 3000
 # removeScaffolds.py is used to filter it.
-python removeScaffolds.py Haemoproteus_tartakovskyi.raw.genome 25 Haemoproteus_tartakovskyi.filter.genome 3000
+python removeScaffolds.py ../resources/genome/Haemoproteus_tartakovskyi.raw.genome 25 ../resources/genome/Haemoproteus_tartakovskyi.filter.genome 3000
 
 # Gene prediction - Genemark
 # using the Haemoproteus tartakovskyi genome
 
-nohup gmes_petap.pl --cores 20 --min_contig 10000 --ES --sequence Haemoproteus_tartakovskyi.filter.genome \
-    > gmes_petap.log 2>&1 &
+nohup gmes_petap.pl --cores 20 --min_contig 10000 --ES --sequence ../resources/genome/Haemoproteus_tartakovskyi.filter.genome \
+    > ..results/01_gene_pred/gmes_petap.Ht.log 2>&1 &
 PID=$!
 wait $PID
-echo "gmes_petap.pl run completed!" | tee -a gmes_petap.log
+echo "gmes_petap.pl run completed!" | tee -a ..results/01_gene_pred/gmes_petap.Ht.log
 
 # --cores: CPU cores to use
 # --min_contig: minimum contig length used for training
 # --ES: Self-training gene prediction
 # PID: Process id (assigned to each running program in Linux)
 # tee: writes the output to both file and the terminal (-a append to the file)
+
+# move the default genemark.gtf to a defined file name
+mv genemark.gtf ..results/01_gene_pred/Ht.gtf
